7c7817352aa72c5b686e2819fad402a76a27fc1c
Sami Tapalinen <t1tasa01@students.oamk.fi>
2023-03-30 21:30:10 +0300

    Add page and data fetching for visual 1
    WIP, fetching does not work yet as intended.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 import Frontpage from './pages/Frontpage';
 import Navbar from './components/Navbar';
+import Visual1 from './pages/Visual1';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Frontpage />} />
+          <Route path="/visual1" element={<Visual1 />} />
         </Routes>
       </div>
     </Router>
