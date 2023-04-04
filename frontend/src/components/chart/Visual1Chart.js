98eda9df8b861f1e5008f5027ad5918c24f23e67
Sami Tapalinen <t1tasa01@students.oamk.fi>
2023-04-05 00:37:49 +0300

    Add visual 1 monthly data to the chart

--- frontend/src/components/chart/Visual1Chart.js
+++ frontend/src/components/chart/Visual1Chart.js
@@ -1,102 +1,193 @@
 import React from 'react';
-import { Chart } from 'chart.js/auto';
 import { Line } from 'react-chartjs-2';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import { DateTime } from 'luxon';
 
-const sortByYear = (a, b) => {
+const sortAnnualData = (a, b) => {
   const n = a.time - b.time;
   if (n !== 0) {
     return n;
   }
 };
 
+const sortMonthlyData = (a, b) => {
+  return a.time.localeCompare(b.time);
+};
+
 const Visual1Chart = () => {
-  
+  const [timeframe, setTimeframe] = useState("Annual");
   const [globalAnnualData, setGlobalAnnualData] = useState([]);
   const [northAnnualData, setNorthAnnualData] = useState([]);
   const [southAnnualData, setSouthAnnualData] = useState([]);
   const [reconstructionData, setReconstructionData] = useState([]);
+  const [globalMonthlyData, setGlobalMonthlyData] = useState([]);
+  const [northMonthlyData, setNorthMonthlyData] = useState([]);
+  const [southMonthlyData, setSouthMonthlyData] = useState([]);
+
+  const filteredData = useMemo(() => {
+    return {
+      Global: {
+        Annual: globalAnnualData,
+        Monthly: globalMonthlyData,
+      },
+      Northern: {
+        Annual: northAnnualData,
+        Monthly: northMonthlyData,
+      },
+      Southern: {
+        Annual: southAnnualData,
+        Monthly: southMonthlyData,
+      },
+      Reconstruction: {
+        Annual: reconstructionData,
+      },
+    };
+  }, [globalAnnualData, northAnnualData, southAnnualData, reconstructionData, globalMonthlyData, northMonthlyData, southMonthlyData]);
 
   useEffect(() => {
+    setTimeframe(timeframe);
     const fetchData = async () => {
       const response = await fetch(process.env.REACT_APP_VISUAL_1_DATA_URL);
       const json = await response.json();
-  
-      setGlobalAnnualData(json.filter(json => json.hemisphere == 'Global' && json.timeframe == 'Annual').sort(sortByYear));
-      setNorthAnnualData(json.filter(json => json.hemisphere == 'Northern' && json.timeframe == 'Annual').sort(sortByYear));
-      setSouthAnnualData(json.filter(json => json.hemisphere == 'Southern' && json.timeframe == 'Annual').sort(sortByYear));
-      setReconstructionData(json.filter(json => json.hemisphere == 'Reconstruction' && json.timeframe == 'Annual').sort(sortByYear));
+
+      setGlobalAnnualData(json.filter(data => data.hemisphere === 'Global' && data.timeframe === 'Annual')
+        .sort(sortAnnualData)
+        .map(data => ({ x: DateTime.fromFormat(data.time, "yyyy"), y: data.temperature }))
+      );
+      setNorthAnnualData(json.filter(json => json.hemisphere === 'Northern' && json.timeframe === 'Annual')
+        .sort(sortAnnualData)
+        .map(data => ({ x: DateTime.fromFormat(data.time, "yyyy"), y: data.temperature }))
+      );
+      setSouthAnnualData(json.filter(json => json.hemisphere === 'Southern' && json.timeframe === 'Annual')
+        .sort(sortAnnualData)
+        .map(data => ({ x: DateTime.fromFormat(data.time, "yyyy"), y: data.temperature }))
+      );
+
+      setReconstructionData(json.filter(json => json.hemisphere === 'Reconstruction' && json.timeframe === 'Annual')
+        .sort(sortAnnualData)
+        .map(data => ({ x: DateTime.fromFormat(data.time, "yyyy"), y: data.temperature }))
+      );
+
+      setGlobalMonthlyData(json.filter(json => json.hemisphere === 'Global' && json.timeframe === 'Monthly')
+        .sort(sortMonthlyData)
+        .map(data => ({ x: DateTime.fromFormat(data.time, "yyyy-MM"), y: data.temperature }))
+      );
+      setNorthMonthlyData(json.filter(json => json.hemisphere === 'Northern' && json.timeframe === 'Monthly')
+        .sort(sortMonthlyData)
+        .map(data => ({ x: DateTime.fromFormat(data.time, "yyyy-MM"), y: data.temperature }))
+      );
+      setSouthMonthlyData(json.filter(json => json.hemisphere === 'Southern' && json.timeframe === 'Monthly')
+        .sort(sortMonthlyData)
+        .map(data => ({ x: DateTime.fromFormat(data.time, "yyyy-MM"), y: data.temperature }))
+      );
     }
 
-    fetchData()
-    .catch(console.error);
-  }, [])
+    fetchData().catch(console.error);
+  }, [timeframe])
 
-    const data = {
+  const TimeframeAnnual = () => {
+    setTimeframe("Annual");
+  };
+
+  const TimeframeMonthly= () => {
+    setTimeframe("Monthly");
+  };
+
+    const annualData = {
       datasets: [
         {
           label: 'Global annual anomalies',
-          data: globalAnnualData,
+          data: filteredData.Global.Annual,
           borderColor: 'rgb(0, 0, 0)',
-          backgroundColor: 'rgb(0, 0, 0, 0.5)',
-          parsing: {
-            xAxisKey: 'time',
-            yAxisKey: 'temperature',
-          },
-          pointRadius: 2,
+          backgroundColor: 'rgb(0, 0, 0, 0.5)'
         },
         {
-            label: 'North annual anomalies',
-            data: northAnnualData,
-            borderColor: 'rgb(255, 0, 0)',
-            backgroundColor: 'rgb(255, 0, 0, 0.5)',
-            parsing: {
-              xAxisKey: 'time',
-              yAxisKey: 'temperature',
-            },
-            pointRadius: 2,
+          label: 'North annual anomalies',
+          data: filteredData.Northern.Annual,
+          borderColor: 'rgb(255, 0, 0)',
+          backgroundColor: 'rgb(255, 0, 0, 0.5)'
         },
         {
-            label: 'South annual anomalies',
-            data: southAnnualData,
-            borderColor: 'rgb(255, 200, 0)',
-            backgroundColor: 'rgb(255, 200, 0, 0.5)',
-            parsing: {
-              xAxisKey: 'time',
-              yAxisKey: 'temperature',
-            },
-            pointRadius: 2,
+          label: 'South annual anomalies',
+          data: filteredData.Southern.Annual,
+          borderColor: 'rgb(255, 200, 0)',
+          backgroundColor: 'rgb(255, 200, 0, 0.5)'
         },
         {
-            label: 'Reconstruction',
-            data: reconstructionData,
-            borderColor: 'rgb(0, 126, 255)',
-            backgroundColor: 'rgb(0, 126, 255, 0.5)',
-            parsing: {
-              xAxisKey: 'time',
-              yAxisKey: 'temperature',
-            },
-            pointRadius: 2,
+          label: 'Reconstruction',
+          data: filteredData.Reconstruction.Annual,
+          borderColor: 'rgb(0, 126, 255)',
+          backgroundColor: 'rgb(0, 126, 255, 0.5)'
+        }
+      ],
+    };
+
+    const monthlyData = {
+      datasets: [
+        {
+          label: 'Global monthly anomalies',
+          data: filteredData.Global.Monthly,
+          borderColor: 'rgb(0, 0, 0)',
+          backgroundColor: 'rgb(0, 0, 0, 0.5)'
+        },
+        {
+          label: 'North monthly anomalies',
+          data: filteredData.Northern.Monthly,
+          borderColor: 'rgb(255, 0, 0)',
+          backgroundColor: 'rgb(255, 0, 0, 0.5)'
+        },
+        {
+          label: 'South monthly anomalies',
+          data: filteredData.Southern.Monthly,
+          borderColor: 'rgb(255, 200, 0)',
+          backgroundColor: 'rgb(255, 200, 0, 0.5)'
         }
       ],
     };
   
     const options = {
       responsive: true,
+      animation: false,
       plugins: {
-        legend: {
-          position: 'top',
+        tooltip: {
+          mode: 'index',
+          intersect: false
         },
         title: {
-          display: false,
+          display: true,
+          text: "HadCRUT5 Temperature Anomaly Analysis"
         },
       },
+      adapters: {
+        date: { id: 'luxon', DateTime },
+      },
       scales: {
-        temperature: {
-          type: 'linear',
-          display: true,
+        x: {
+          type: 'time',
+          time: {
+            unit: 'year',
+          },
           position: 'bottom',
+          title:{
+            display: true,
+            text: "Date",
+          }
         },
+        y: {
+          type: "linear",
+          position: "left",
+          title: {
+            display: true,
+            text: "Temperature Anomaly",
+          },
+          min: -1.5,
+          max: 1.5
+        },
+      },
+      elements: {
+        point:{
+            radius: 0
+        }
       },
     };
   
@@ -104,21 +195,18 @@ const Visual1Chart = () => {
       <div style={{ width: '95%' }}>
         <div className='container-visual-radios'>
           <div className='radio'>
-            <label>
-              <input type='radio' value='annual' name='timeframe' defaultChecked />
-              Annual
-            </label>
+          <input type='radio' id='annual' name='timeframe' defaultChecked onClick={TimeframeAnnual}/>
+          <label htmlFor='annual'>Annual</label>
           </div>
           <div className='radio'>
-            <label>
-              <input type='radio' value='monthly' name='timeframe' />
-              Monthly
-            </label>
+          <input type='radio' id='monthly' name='timeframe' onClick={TimeframeMonthly} />
+            <label htmlFor='monthly'>Monthly</label>
           </div>
         </div>
-        <Line options={options} data={data} />
+        {timeframe === "Annual" && <Line data={annualData} options={options} />}
+        {timeframe === "Monthly" && <Line data={monthlyData} options={options} />}
       </div>
     );
   };
-
+//<Line options={options} data={timeframe === "Annual" ? annualData : monthlyData} />
   export default Visual1Chart;
