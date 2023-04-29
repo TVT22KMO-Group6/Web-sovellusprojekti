6f0c7b2065196524f2e689d706f5af12457816c3
Mika Sarajärvi <mika.sarajarvi@gmail.com>
2023-04-29 15:34:35 +0300

    Tekstit skaalautuu

--- frontend/src/pages/Frontpage.js
+++ frontend/src/pages/Frontpage.js
@@ -20,11 +20,14 @@ function FrontPage() {
       <div className="background-image" style={{backgroundImage: `url(${natureImage})`}}></div>
       <div className="content">
         <h1 className="text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ultrices eros in cursus turpis. Morbi tristique senectus et netus et malesuada fames ac.
-          Tortor posuere ac ut consequat semper viverra nam.
-          Vestibulum lectus mauris ultrices eros. Morbi tristique senectus et netus.
+        Climate change refers to long-term shifts in temperatures and weather patterns. These shifts may be natural, such as through variations in the solar cycle. But since the 1800s, human activities have been the main driver of climate change, primarily due to burning fossil fuels like coal, oil and gas.
+        Burning fossil fuels generates greenhouse gas emissions that act like a blanket wrapped around the Earth, trapping the sun’s heat and raising temperatures.
+        Examples of greenhouse gas emissions that are causing climate change include carbon dioxide and methane. These come from using gasoline for driving a car or coal for heating a building, for example. Clearing land and forests can also release carbon dioxide. Landfills for garbage are a major source of methane emissions. Energy, industry, transport, buildings, agriculture and land use are among the main emitters.
+
+        And emissions continue to rise. As a result, the Earth is now about 1.1°C warmer than it was in the late 1800s. The last decade (2011-2020) was the warmest on record.
+        Many people think climate change mainly means warmer temperatures. But temperature rise is only the beginning of the story. Because the Earth is a system, where everything is connected, changes in one area can influence changes in all others.
+        The consequences of climate change now include, among others, intense droughts, water scarcity, severe fires, rising sea levels, flooding, melting polar ice, catastrophic storms and declining biodiversity. These changes are already happening and are expected to become more frequent and intense in the coming decades.
+
         </h1>
       </div>
     </main>
